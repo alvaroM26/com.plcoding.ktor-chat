00345fe24fe6899a2cd457996d17abca9516553e
Alvaro <alvmol706@alu.centroafuera.es>
2022-11-18 13:16:05 +0100

    Servidor WebSocket

--- src/main/kotlin/com/plcoding/Application.kt
+++ src/main/kotlin/com/plcoding/Application.kt
@@ -9,7 +9,7 @@ import org.koin.ktor.ext.modules
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+@Suppress("unused")
 fun Application.module() {
     install(Koin) {
         modules(mainModule)
